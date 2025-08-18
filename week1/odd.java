a9777233db69b485ac2fc08332f136656cf2a603
reshabi126 <reshabi126@gmail.com>
2025-08-18 11:35:15 +0530

    Add files via upload

---
+++ week1/odd.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class odd {
+
+	public static void main(String[] args) {
+		for(int i =0;i<10;i++)
+		{
+			if(i%2 ==1)
+			{
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
